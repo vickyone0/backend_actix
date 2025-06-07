6e9a209a0cf437410d8e9a3bc32b96670013df32
vickyone0 <vigneshs0.618@gmail.com>
2025-06-07 00:27:23 -0400

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use actix_web::{http::Method, middleware, web, App, HttpResponse, HttpServer};
+use actix_web::middleware::{ErrorHandlerResponse, ErrorHandlers};
+use actix_web::{
+    dev,
+    http::{header, StatusCode},
+    web, App, HttpResponse, HttpServer, Result,
+};
+
+
+fn add_error_header<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    res.response_mut().headers_mut().insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_static("Error"),
+    );
+
+    Ok(ErrorHandlerResponse::Response(res.map_into_left_body()))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .wrap(middleware::DefaultHeaders::new().add(("X-version", "0.2")))
-            .service(
-                web::resource("/test")
-                    .route(web::get().to(HttpResponse::Ok))
-                    .route(web::method(Method::HEAD).to(HttpResponse::MethodNotAllowed))
+            .wrap(
+                ErrorHandlers::new()
+                    .handler(StatusCode::INTERNAL_SERVER_ERROR, add_error_header),
             )
+            .service(web::resource("/").route(web::get().to(HttpResponse::InternalServerError)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
