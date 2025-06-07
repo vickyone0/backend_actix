7d6a7fea424a0f56c18459974a49aa4a17200878
vickyone0 <vigneshs0.618@gmail.com>
2025-06-07 04:52:47 -0400

    upload image to memory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use actix_web::{post,web, App, HttpResponse, HttpServer, Result, middleware::Logger};
-use serde::Deserialize;
+use actix_web::{web, App, HttpResponse, HttpServer, Result, middleware::Logger};
+use actix_multipart::Multipart;
+use futures_util::StreamExt as _;
+use std::io::Write;
+use sanitize_filename::sanitize;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -8,26 +11,32 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .route("/", web::get().to(index))
-            .service(submit)
+            .route("/upload", web::post().to(upload_file))
+
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-
 async fn index() -> HttpResponse {
     HttpResponse::Ok().body("vignesh")
 }
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
-}
+async fn upload_file(mut payload: Multipart) -> Result<HttpResponse> {
+    while let Some(item) = payload.next().await {
+        let mut field = item?;
+        let content_disposition = field.content_disposition();
+        let filename = content_disposition.get_filename().unwrap_or("upload.bin");
+        let filepath = format!("./uploads/{}",sanitize(&filename));
+        let mut f = std::fs::File::create(filepath)?;
 
+        while let Some(chunk) = field.next().await {
+            let data = chunk?;
+            f.write_all(&data)?;
+            
+        }
+    }
 
-#[post("/submit")]
-async fn submit(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
-
+    Ok(HttpResponse::Ok().body("file uploaded"))
 }
